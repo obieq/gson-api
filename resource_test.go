d0e2ea5208d63b35df9f7c46b50e2cc31ffb8d6e
obieq <obie.quelland@pnldata.com>
2015-07-11 19:38:18 -0400

    [#98883882] resource now has 100% test coverage

--- resource_test.go
+++ resource_test.go
@@ -1,13 +1,40 @@
 package gsonapi
 
 import (
+	validations "github.com/obieq/goar-validations"
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Resource", func() {
-	var ()
+	var (
+		errors *map[string]*validations.ValidationError
+	)
 
 	BeforeEach(func() {
+		errorList := map[string]*validations.ValidationError{}
+		veYear := validations.ValidationError{Key: "year", Message: "cannot be greater than 2016"}
+		veMake := validations.ValidationError{Key: "make", Message: "cannot be blank"}
+		errorList[veYear.Key] = &veYear
+		errorList[veMake.Key] = &veMake
+
+		errors = &errorList
+	})
+
+	Context("Errors", func() {
+		It("should set errors", func() {
+			r := AutomobileResource{}
+			r.SetErrors(*errors)
+		})
+
+		It("should get errors", func() {
+			r := AutomobileResource{}
+			Ω(len(r.Errors())).Should(Equal(0))
+
+			r.SetErrors(*errors)
+
+			// verify
+			Ω(len(r.Errors())).Should(Equal(2))
+		})
 	})
 })
