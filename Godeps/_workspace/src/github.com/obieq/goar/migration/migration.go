79a939a849350e1e24b36183bbf449d9ba6b5eff
obieq <obie.quelland@pnldata.com>
2015-07-15 08:17:16 -0400

    updated godep

--- Godeps/_workspace/src/github.com/obieq/goar/migration/migration.go
+++
@@ -1,9 +0,0 @@
-package migration
-
-type Migrator interface {
-	CreateDb(dbName string) error
-	DropDb(dbName string) error
-	CreateTable(tableName string) error
-	DropTable(tableName string) error
-	AddIndex(tableName string, fields []string, opts map[string]interface{}) error
-}
